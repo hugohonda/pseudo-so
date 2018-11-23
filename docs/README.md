5f76d664aeddb227e792b165db95875df006cbe0
William <williamsimiao@gmail.com>
2018-11-23 18:32:41 -0200

    starting the queue module

--- docs/README.md
+++ docs/README.md
@@ -3,3 +3,4 @@ Final Project - Operational systems - 2/2018
 --------------
 
 - Hugo Honda Ferreira - 12/0012979
+- William Simião - 13/0138002
--- modules/queue.py
+++ modules/queue.py
@@ -4,3 +4,35 @@
 
 # fila de processos de usuários - múltiplas filas de prioridades com realimentação
 #   - três filas com prioridades distintas
+class Queue:
+    def __init__(self, priority):
+        self.fila = []
+        self.priority = priority
+
+class queue_interface:
+    
+    def __init__(self, queue, priority):
+        self.real_time = new Queue(0)
+        self.user_p1 = new Queue(1)
+        self.user_p2 = new Queue(2)
+        self.user_p3 = new Queue(3)
+
+    def add_to_queue(self, process):
+        if(process.priority == 0):
+            self.real_time.append(process)
+        elif(process.priority == 1):
+            self.user_p1.fila.append(process)
+        elif(process.priority == 2):
+            self.user_p2.fila.append(process)
+        elif(process.priority == 3):
+            self.user_p3.fila.append(process)
+    
+    def pop_in_priority_order(self):
+        if(self.real_time != []):
+            return self.real_time.fila.pop()
+        if(self.user_p1 != []):
+            return self.user_p1.fila.pop()
+        if(self.user_p2 != []):
+            return self.user_p2.fila.pop()
+        if(self.user_p3 != []):
+            return self.user_p3.fila.pop()
